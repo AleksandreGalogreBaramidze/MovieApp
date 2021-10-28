0e62e1756ba3e5c882922ca5b4f952401de6f494
Aleksandre Galogre-Baramidze <74540578+AleksandreGalogreBaramidze@users.noreply.github.com>
2021-10-28 15:34:02 +0400

    conflict fixed

--- app/src/main/java/com/example/movieapp/api/RetrofitRepository.kt
+++ app/src/main/java/com/example/movieapp/api/RetrofitRepository.kt
@@ -3,7 +3,6 @@ package com.example.movieapp.api
 import com.example.movieapp.models.Detail
 import com.example.movieapp.models.Movie
 
-
 interface RetrofitRepository {
     suspend fun getPopularMoviesData(page: Int): ApiErrorHandling<Movie>
     suspend fun getTopMoviesData(page: Int): ApiErrorHandling<Movie>
